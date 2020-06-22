e057f8d640bcd651b18b386ad72f641b62f3b2cc
DK <dknguyen.work@gmail.com>
2020-06-22 16:23:40 +0300

    rerunning experiments

--- batchscripts/batchscript_array.sh
+++ batchscripts/batchscript_array.sh
@@ -7,15 +7,28 @@
 #SBATCH --nodes 1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:teslav100:1
-#SBATCH --array=1-2
+#SBATCH --array=1-15
 #SBATCH -t 5-23:59:00
 
 export PYTHONPATH=$PYTHONPATH:.
 source activate audio-captioning
 
 case $SLURM_ARRAY_TASK_ID in
-  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/attn_2_512_lr_1e-4_loss_thr_1e-3_class_weight_0.5 -v;;
-  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/attn_2_512_lr_1e-4_loss_thr_1e-3_class_weight_0.5 -v;;
+  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
+  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_no_attn_lr_1e-4_loss_thr_1e-3_clamp_0.5 -v;;
+  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_no_attn_lr_1e-4_loss_thr_1e-3 -v;;
+  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
+  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_no_attn_lr_1e-4_loss_thr_1e-3_clamp_0.5 -v;;
+  6) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_no_attn_lr_1e-4_loss_thr_1e-3 -v;;
+  7) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
+  8) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_no_attn_lr_1e-4_loss_thr_1e-3_clamp_0.5 -v;;
+  9) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_no_attn_lr_1e-4_loss_thr_1e-3 -v;;
+  10) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
+  11) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_no_attn_lr_1e-4_loss_thr_1e-3_clamp_0.5 -v;;
+  12) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_no_attn_lr_1e-4_loss_thr_1e-3 -v;;
+  13) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
+  14) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_no_attn_lr_1e-4_loss_thr_1e-3_clamp_0.5 -v;;
+  15) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_no_attn_lr_1e-4_loss_thr_1e-3 -v;;
 esac
 
 echo Done!
