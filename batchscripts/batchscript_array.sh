e04579612fede63aaec4cf0af2651855d10757df
DK <dknguyen.work@gmail.com>
2020-06-23 12:31:55 +0300

    modify batch script

--- batchscripts/batchscript_array.sh
+++ batchscripts/batchscript_array.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes 1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:teslav100:1
-#SBATCH --array=1-10
+#SBATCH --array=1-2
 #SBATCH -t 5-23:59:00
 
 export PYTHONPATH=$PYTHONPATH:.
@@ -16,14 +16,6 @@ source activate audio-captioning
 case $SLURM_ARRAY_TASK_ID in
   1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
   2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_no_attn_lr_1e-4_loss_thr_1e-4_clamp_0.5 -v;;
-  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
-  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_no_attn_lr_1e-4_loss_thr_1e-4_clamp_0.5 -v;;
-  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
-  6) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_no_attn_lr_1e-4_loss_thr_1e-4_clamp_0.5 -v;;
-  7) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
-  8) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_no_attn_lr_1e-4_loss_thr_1e-4_clamp_0.5 -v;;
-  9) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_no_attn_lr_1e-4_loss_thr_1e-4 -v;;
-  10) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_no_attn_lr_1e-4_loss_thr_1e-4_clamp_0.5 -v;;
 esac
 
 echo Done!
