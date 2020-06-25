c526570ef31ee78281cd536b0d9f34a0de23a68d
DK <dknguyen.work@gmail.com>
2020-06-25 12:08:00 +0300

    change batc size to 8

---
+++ batchscripts/batchscript_array_baseline_clamp_0.5.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#SBATCH -J "AudioCap"
+#SBATCH -o outputs/outputs_run/out_%A_%a.txt
+#SBATCH -e outputs/errs_run/err_%A_%a.txt
+#SBATCH --mem-per-cpu=70000
+#SBATCH --nodes 1
+#SBATCH -p gpu
+#SBATCH --gres=gpu:teslav100:1
+#SBATCH --array=1-5
+#SBATCH -t 5-23:59:00
+
+export PYTHONPATH=$PYTHONPATH:.
+source activate audio-captioning
+
+case $SLURM_ARRAY_TASK_ID in
+  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_params_same_baseline_clamp_0.5_batchsize_8_nodecdropout -v;;
+  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_params_same_baseline_clamp_0.5_batchsize_8_nodecdropout -v;;
+  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_params_same_baseline_clamp_0.5_batchsize_8_nodecdropout -v;;
+  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_params_same_baseline_clamp_0.5_batchsize_8_nodecdropout-v;;
+  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_params_same_baseline_clamp_0.5_batchsize_8_nodecdropout -v;;
+esac
+
+echo Done!
