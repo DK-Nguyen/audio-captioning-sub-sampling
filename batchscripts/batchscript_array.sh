67b51b025e94c53d3d5d3cad01fe73688ad23694
DK <dknguyen.work@gmail.com>
2020-06-25 12:20:20 +0300

    training with no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout/

--- batchscripts/batchscript_array.sh
+++ batchscripts/batchscript_array.sh
@@ -14,11 +14,11 @@ export PYTHONPATH=$PYTHONPATH:.
 source activate audio-captioning
 
 case $SLURM_ARRAY_TASK_ID in
-  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_nodecdropout -v;;
-  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_nodecdropout -v;;
-  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_nodecdropout -v;;
-  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_nodecdropout -v;;
-  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_nodecdropout -v;;
+  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
+  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
+  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
+  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
+  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
 esac
 
 echo Done!
