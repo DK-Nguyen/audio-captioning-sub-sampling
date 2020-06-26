13937ec1646daf7d96a88bfa8ff3bc5907d0daa7
DK <dknguyen.work@gmail.com>
2020-06-26 15:23:29 +0300

    add dropout in decoder again

--- batchscripts/batchscript_array.sh
+++ batchscripts/batchscript_array.sh
@@ -7,18 +7,18 @@
 #SBATCH --nodes 1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:teslav100:1
-#SBATCH --array=1-5
+#SBATCH --array=1-4
 #SBATCH -t 5-23:59:00
 
 export PYTHONPATH=$PYTHONPATH:.
 source activate audio-captioning
 
 case $SLURM_ARRAY_TASK_ID in
-  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
-  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
-  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
-  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
-  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_no_attn_lr_1e-4_lossthr_1e-3_noval_freqclamp_0.5_gradnorm_0.5_epochs_1000_patience_100_batchsize_8_nodecdropout -v;;
+  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_params_same_baseline_hasdecdropout_epoch_1000 -v;;
+  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_params_same_baseline_hasdecdropout_epoch_1000 -v;;
+  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_params_same_baseline_hasdecdropout_epoch_1000 -v;;
+  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_params_same_baseline_hasdecdropout_epoch_1000 -v;;
+
 esac
 
 echo Done!
