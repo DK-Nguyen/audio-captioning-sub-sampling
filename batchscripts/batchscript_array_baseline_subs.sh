408520110992a4c72bf23b01c341a899dd4836ed
DK <dknguyen.work@gmail.com>
2020-06-26 10:39:36 +0300

    training with same baseline params with 1000 epochs

---
+++ batchscripts/batchscript_array_baseline_subs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#SBATCH -J "AudioCap"
+#SBATCH -o outputs/outputs_run/out_%A_%a.txt
+#SBATCH -e outputs/errs_run/err_%A_%a.txt
+#SBATCH --mem-per-cpu=70000
+#SBATCH --nodes 1
+#SBATCH -p gpu
+#SBATCH --gres=gpu:teslav100:1
+#SBATCH --array=1-4
+#SBATCH -t 5-23:59:00
+
+export PYTHONPATH=$PYTHONPATH:.
+source activate audio-captioning
+
+case $SLURM_ARRAY_TASK_ID in
+  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2_params_same_baseline_nodecdropout_epoch_1000 -v;;
+  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4_params_same_baseline_nodecdropout_epoch_1000 -v;;
+  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8_params_same_baseline_nodecdropout_epoch_1000 -v;;
+  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16_params_same_baseline_nodecdropout_epoch_1000 -v;;
+esac
+
+echo Done!
