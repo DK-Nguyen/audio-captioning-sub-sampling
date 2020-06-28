7d6bfdb653cb3a691758977eb90ef239c08d660f
DK <dknguyen.work@gmail.com>
2020-06-28 16:59:55 +0300

    training with load into memory

---
+++ batchscripts/batchscript_sub1.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#SBATCH -J "AudioCap"
+#SBATCH -o outputs/outputs_run/out_%A_%a.txt
+#SBATCH -e outputs/errs_run/err_%A_%a.txt
+#SBATCH --mem-per-cpu=70000
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --nodes 1
+#SBATCH -p gpu
+#SBATCH --gres=gpu:teslav100:1
+#SBATCH -t 5-23:59:00
+
+export PYTHONPATH=$PYTHONPATH:.
+source activate audio-captioning
+
+python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1_params_same_baseline_hasdecdropout_epoch_1000 -v
+
+echo Done!
