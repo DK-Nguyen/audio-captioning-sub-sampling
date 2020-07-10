4ae022e10ef7b700c10af7b9f26df8dcc7d2f720
DK <dknguyen.work@gmail.com>
2020-07-10 20:02:15 +0300

    training rnn-subsampling

--- batchscripts/batchscript_array.sh
+++ batchscripts/batchscript_array.sh
@@ -7,23 +7,18 @@
 #SBATCH --nodes 1
 #SBATCH -p gpu
 #SBATCH --gres=gpu:teslav100:1
-#SBATCH --array=1-10
+#SBATCH --array=1-5
 #SBATCH -t 5-23:59:00
 
 export PYTHONPATH=$PYTHONPATH:.
 source activate audio-captioning
 
 case $SLURM_ARRAY_TASK_ID in
-  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1linear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2linear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4linear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8linear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16linear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  6) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1maxoutlinear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  7) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2maxoutlinear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  8) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4maxoutlinear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  9) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8maxoutlinear_same_training_params_baseline_epoch_1000_no_attention -v;;
-  10) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16maxoutlinear_same_training_params_baseline_epoch_1000_no_attention -v;;
+  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1rnn_same_training_params_baseline_epoch_1000_no_attention -v;;
+  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2rnn_same_training_params_baseline_epoch_1000_no_attention -v;;
+  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4rnn_same_training_params_baseline_epoch_1000_no_attention -v;;
+  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8rnn_same_training_params_baseline_epoch_1000_no_attention -v;;
+  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16rnn_same_training_params_baseline_epoch_1000_no_attention -v;;
 esac
 
 echo Done!
