bf507cca3d7f0827859b301b3be50be780c9a8d0
DK <dknguyen.work@gmail.com>
2020-09-09 16:05:44 +0300

    training dropping subsampling with 100 patience, 0.5 frequency clamp

--- batchscripts/batchscript_array.sh
+++ batchscripts/batchscript_array.sh
@@ -14,11 +14,11 @@ export PYTHONPATH=$PYTHONPATH:.
 source activate audio-captioning
 
 case $SLURM_ARRAY_TASK_ID in
-  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1Dropping_1000epochs_10patience_0.5classWeights -v;;
-  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2Dropping_1000epochs_10patience_0.5classWeights -v;;
-  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4Dropping_1000epochs_10patience_0.5classWeights -v;;
-  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8Dropping_1000epochs_10patience_0.5classWeights -v;;
-  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16Dropping_1000epochs_10patience_0.5classWeights -v;;
+  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1Dropping_1000epochs_100patience_0.5classWeights -v;;
+  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2Dropping_1000epochs_100patience_0.5classWeights -v;;
+  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4Dropping_1000epochs_100patience_0.5classWeights -v;;
+  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8Dropping_1000epochs_100patience_0.5classWeights -v;;
+  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16Dropping_1000epochs_100patience_0.5classWeights -v;;
 esac
 
 echo Done!
