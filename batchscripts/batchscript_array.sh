98dcfeb87fa3df13ed92b3668a0ea8ead4f10523
DK <dknguyen.work@gmail.com>
2020-09-09 15:43:41 +0300

    rerun experiments with dropping subsampling, together with use_class_weights of 0.5

--- batchscripts/batchscript_array.sh
+++ batchscripts/batchscript_array.sh
@@ -14,29 +14,11 @@ export PYTHONPATH=$PYTHONPATH:.
 source activate audio-captioning
 
 case $SLURM_ARRAY_TASK_ID in
-  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/no_attention/sub1Dropping_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/no_attention/sub2Dropping_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/no_attention/sub4Dropping_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/no_attention/sub8Dropping_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/no_attention/sub16Dropping_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-
-  6) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/no_attention/sub1Linear_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  7) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/no_attention/sub2Linear_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  8) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/no_attention/sub4Linear_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  9) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/no_attention/sub8Linear_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  10) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/no_attention/sub16Linear_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-
-  11) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/no_attention/sub1Maxout_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  12) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/no_attention/sub2Maxout_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  13) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/no_attention/sub4Maxout_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  14) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/no_attention/sub8Maxout_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  15) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/no_attention/sub16Maxout_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-
-  16) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/no_attention/sub1Rnn_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  17) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/no_attention/sub2Rnn_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  18) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/no_attention/sub4Rnn_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  19) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/no_attention/sub8Rnn_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
-  20) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/no_attention/sub16Rnn_sameTrainingParamsBaseline_1000epochs_noAttention -v;;
+  1) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling1/sub1Dropping_1000epochs_10patience_0.5classWeights -v;;
+  2) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling2/sub2Dropping_1000epochs_10patience_0.5classWeights -v;;
+  3) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling4/sub4Dropping_1000epochs_10patience_0.5classWeights -v;;
+  4) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling8/sub8Dropping_1000epochs_10patience_0.5classWeights -v;;
+  5) python main.py -c main_settings -j ${SLURM_JOBID} -d settings/subsampling16/sub16Dropping_1000epochs_10patience_0.5classWeights -v;;
 esac
 
 echo Done!
